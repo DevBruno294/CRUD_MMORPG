775bffa58cc5481ae5ecf39b372e9eed670b1a19
Bruno Acosta <acostabruno35@gmail.com>
2026-04-04 19:29:09 -0300

    Agrego CDN de Boostrap, titulo y boton de Nuevo Heroe

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php 
-// inicio del proyecto
-?>
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forja de Heroes</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container my-5">
+        <h2>Lista de Heroes</h2>
+        <a class="btn btn-primary" href="" role="button">Nuevo Heroe</a>
+    </div>
+</body>
+</html>
